d76babed58be717632a10939d9b6115af04f2a11
Thomas Habets <thomas@habets.se>
2023-01-17 22:52:32 +0000

    Remove old test instructions

--- test.sh
+++ test.sh
@@ -17,24 +17,21 @@ fi
 rm -f scratch/*
 
 echo "============================================="
-echo "Choose any password"
 ./testspect_gen.ex ./kybertest_keygen -o scratch/testkey
 ./kybertest_encrypt -F 1 -r scratch/testkey.pub < testdata/plain.txt > scratch/enc2
 ls -l scratch/enc2
 
 echo "============================================="
-echo "Enter it again"
 ./testspect.ex ./kybertest_decrypt scratch/testkey.priv scratch/enc2
 
 echo "============================================="
 echo "Backwards compatability check"
-echo "Enter 'secret' as the password every time you're asked"
 # Backwards compat check.
 CORRECT="$(sha1sum testdata/plain.txt | awk '{print $1}')"
 for key in 0 1 2 3; do
     for i in testdata/encrypted*key${key?}.kyb; do
 	KEYFN="$(ls testdata/testkey_v?_key${key?}.priv)"
-	T="$(./testspect.ex ./kybertest_decrypt "${KEYFN?}" $i)"
+	./testspect.ex ./kybertest_decrypt "${KEYFN?}" $i
     done
 done
 
