8dbd67295c04ab882f363f7a362d4235cde9f270
Thomas Habets <thomas@habets.se>
2023-01-18 21:09:30 +0000

    Improve tests

--- test.sh
+++ test.sh
@@ -4,31 +4,32 @@ set -e
 export ASAN_OPTIONS=detect_stack_use_after_return=1:abort_on_error=1
 #:fast_unwind_on_malloc=0:detect_leaks=0
 
-if false; then
+if true; then
+    echo "====== Rebuilding ==========================="
     make clean || true
     ./bootstrap.sh
     ./configure \
 	PQLIBPATH="$HOME/opt/kyber/lib" \
 	STD=c++17 \
 	CXXFLAGS="-fsanitize=address -fsanitize=undefined"
-    make -j
+    make -j$(nproc)
     make check
 fi
 rm -f scratch/*
 
-echo "============================================="
+echo "====== Generate key and encrypt ============="
 ./testspect_gen.ex ./kybertest_keygen -o scratch/testkey
-./kybertest_encrypt -F 1 -r scratch/testkey.pub < testdata/plain.txt > scratch/enc2
+ls -l scratch/testkey*
+./kybertest_encrypt -r scratch/testkey.pub < testdata/plain.txt > scratch/enc2
 ls -l scratch/enc2
 
-echo "============================================="
+echo "====== Test decrypt ========================="
 ./testspect.ex ./kybertest_decrypt scratch/testkey.priv scratch/enc2
 
-echo "============================================="
-echo "Backwards compatability check"
+echo "====== Backwards compatibility check ========"
 # Backwards compat check.
 CORRECT="$(sha1sum testdata/plain.txt | awk '{print $1}')"
-for key in 0 1 2 3; do
+for key in 0 1 2 3 4 5; do
     for i in testdata/encrypted*key${key?}.kyb; do
 	KEYFN="$(ls testdata/testkey_v?_key${key?}.priv)"
 	./testspect.ex ./kybertest_decrypt "${KEYFN?}" $i
--- testspect.ex
+++ testspect.ex
@@ -5,6 +5,7 @@ set key [lindex $argv 1]
 set file [lindex $argv 2]
 
 spawn $bin -k $key -f $file
+log_user 0
 expect {
     -re "Private key password:|enter AES-256-CBC decryption password:" {
 	send "secret\r"
@@ -13,14 +14,17 @@ expect {
     }
     EOF
 }
+log_user 1
 lassign [wait] pid spawnid os_error_flag value
 if {$value != 0} {
     exit $value
 }
 set payload $expect_out(buffer)
 
+log_user 0
 spawn cat testdata/plain.txt
 expect EOF
+log_user 1
 set golden $expect_out(buffer)
 
 if {$payload != $golden} {
